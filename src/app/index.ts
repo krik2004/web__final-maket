c0fa9e9adc78ebab4aae33ae39020365f7d47c72
krik2004 <157965534+krik2004@users.noreply.github.com>
2024-03-04 15:29:50 +0800

    тест3

--- src/app/index.ts
+++
@@ -1,7 +0,0 @@
-console.log('it works!')
-
-// core app styles:
-import './index.scss'
-import './index.js'
-
-// here you can init react or vue
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-// entry point
-// DO NOT mess this file!
-
-import './app'
-
